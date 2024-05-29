24897c6cb8762c2f97b2dc412821191cce7c51e3
Razi Khan <razikha1999@gmail.com>
2024-05-30 02:05:24 +0500

    fixed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({ children }: { children: any }) {
 						{children}
 					</ClientProviders>
 				</StyledComponentsRegistry>
+				{/* @ts-ignore */}
 				<SmokeEffect />
 			</body>
 		</html>
